f058a86b215ba807078147ecaa4306e51ca288aa
ztNFny <github.com@larskleinschmidt.de>
2018-12-30 12:23:11 +0100

    update versions to make this compile again (sdk 26)

--- mobile/src/main/java/com/javadog/cgeowear/MobileService.java
+++ mobile/src/main/java/com/javadog/cgeowear/MobileService.java
@@ -36,8 +36,8 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.wearable.Node;
 import com.google.android.gms.wearable.NodeApi;
 import com.google.android.gms.wearable.Wearable;
