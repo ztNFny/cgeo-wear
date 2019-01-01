b934a2acc9467c6fe3497b91ec7ff04da424e01b
ztNFny <ztNFny@users.noreply.github.com>
2019-01-01 13:20:26 +0100

    cleanup

--- mobile/src/main/java/com/javadog/cgeowear/OsmAndProxy.java
+++ mobile/src/main/java/com/javadog/cgeowear/OsmAndProxy.java
@@ -22,22 +22,12 @@ public class OsmAndProxy extends Activity {
                 Double latitude = Double.parseDouble(data.getQueryParameter("lat"));
                 Double longitude = Double.parseDouble(data.getQueryParameter("lon"));
                 String cacheName = data.getQueryParameter("name");
-                String geocode = data.getQueryParameter("name");
-
 
                 final Intent launchIntent = new Intent(INTENT_ACTION);
                 launchIntent.setPackage(INTENT_PACKAGE);
                 launchIntent.putExtra(MobileService.EXTRA_CACHE_NAME, cacheName)
-                        .putExtra(MobileService.EXTRA_GEOCODE, geocode)
-                        // original c:geo
-                        //.putExtra(Intents.EXTRA_LATITUDE, coords.getLatitude())
-                        //.putExtra(Intents.EXTRA_LONGITUDE, coords.getLongitude());
-                        // original c:geo + hardcoded strings removed inbetween
                         .putExtra(MobileService.EXTRA_LATITUDE, latitude)
-                        .putExtra(MobileService.EXTRA_LONGITUDE, longitude)
-                // align with https://github.com/ztNFny/cgeo-wear/commit/64c0880068f104fb48c11c48d3c71ac8e67446d1
-                //.putExtra(Intents.EXTRA_COORDS, coords.getCoords());
-                ;
+                        .putExtra(MobileService.EXTRA_LONGITUDE, longitude);
                 getApplicationContext().startService(launchIntent);
             }
         }
